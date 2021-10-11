a87b5322d9421dd5aecfbb099471857f32053989
leen-neel <and24903@gmail.com>
2021-10-11 12:25:48 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+export default defineComponent({
+  name: "App",
+
+  setup() {
+    const quasar = useQuasar();
+
+    quasar.dark.set(true);
+  },
+});
+</script>
