8964329d5f557c491c0d929a13775ef64832ec49
leen-neel <and24903@gmail.com>
2021-10-11 21:01:12 +0530

    added meta

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,8 +24,42 @@
 
 <script>
 import { defineComponent } from "vue";
+import { useMeta } from "quasar";
 
 export default defineComponent({
   name: "MainLayout",
+
+  setup() {
+    useMeta(() => {
+      return {
+        title: "Anindo Neel Dutta | Full-Stack Web Developer",
+        meta: {
+          desicription: {
+            name: "description",
+            content:
+              "Hey there! I am Anindo Neel Dutta! I hail from India. I am a project manager at Norian Games Pvt. Ltd. I make web apps with the MEVN stack.",
+          },
+          ogTitle: {
+            name: "og:title",
+            content: "Anindo Neel Dutta | Full-Stack Web Developer",
+          },
+          ogDescription: {
+            name: "og:description",
+            content:
+              "Hey there! I am Anindo Neel Dutta! I hail from India. I am a project manager at Norian Games Pvt. Ltd. I make web apps with the MEVN stack.",
+          },
+          ogImage: {
+            name: "og:image",
+            content:
+              "https://cdn.discordapp.com/attachments/828659419277426728/897032272023281694/08c57f8e-611d-44e3-b2f1-46470ba293f4-1.jpg",
+          },
+          themeColor: {
+            name: "theme-color",
+            content: "#26a69a",
+          },
+        },
+      };
+    });
+  },
 });
 </script>
