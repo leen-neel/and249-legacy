9ab16a5f4fbc74ed345a1d9ed87d0eb3c1c3e6ea
leen-neel <and24903@gmail.com>
2021-10-11 17:05:30 +0530

    added tabs layout to add texts about me

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -21,24 +21,60 @@
       </div>
     </q-card>
 
-    <q-card class="q-mt-lg">
+    <q-card class="q-mt-lg q-pa-sm">
       <q-card-section>
         <div class="q-mt-xl allign-left text-bold text-h3">About me</div>
       </q-card-section>
       <q-card-section class="about-text">
-        Hey there! I am <span class="archDau"> Anindo Neel Dutta! </span> I am a
-        web app developer working on the <strong>MEVN</strong> stack. I love
-        creating web apps and websites.
+        Hey there! I am
+        <span class="archDau text-bold"> Anindo Neel Dutta! </span> I am a
+        project manager at Norian Games Pvt. Ltd. I make web apps with the
+        <strong>MEVN</strong> stack. I love creating web apps and websites.
       </q-card-section>
+    </q-card>
+
+    <q-card class="q-mt-lg">
+      <q-tabs v-model="tab" dense align="justify">
+        <q-tab name="mails" label="Mails" />
+        <q-tab name="alarms" label="Alarms" />
+        <q-tab name="movies" label="Movies" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="mails">
+          <div class="text-h6">Mails</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="alarms">
+          <div class="text-h6">Alarms</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="movies">
+          <div class="text-h6">Movies</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
     </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "PageIndex",
+
+  setup() {
+    const tab = ref("mails");
+
+    return {
+      tab,
+    };
+  },
 });
 </script>
 
