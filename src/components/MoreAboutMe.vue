3d7c75f7497dd5324f0bcd005cd1b0177ac74d30
leen-neel <and24903@gmail.com>
2021-10-11 17:13:56 +0530

    added more about me component

---
+++ src/components/MoreAboutMe.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-card class="q-mt-lg">
+    <q-tabs v-model="tab" dense align="justify">
+      <q-tab name="mails" label="Mails" />
+      <q-tab name="alarms" label="Alarms" />
+      <q-tab name="movies" label="Movies" />
+    </q-tabs>
+
+    <q-separator />
+
+    <q-tab-panels v-model="tab" animated>
+      <q-tab-panel name="mails">
+        <div class="text-h6">Mails</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="alarms">
+        <div class="text-h6">Alarms</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="movies">
+        <div class="text-h6">Movies</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+    </q-tab-panels>
+  </q-card>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const tab = ref("mails");
+
+    return {
+      tab,
+    };
+  },
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,40 +4,16 @@
 
     <AboutMe />
 
-    <q-card class="q-mt-lg">
-      <q-tabs v-model="tab" dense align="justify">
-        <q-tab name="mails" label="Mails" />
-        <q-tab name="alarms" label="Alarms" />
-        <q-tab name="movies" label="Movies" />
-      </q-tabs>
-
-      <q-separator />
-
-      <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="mails">
-          <div class="text-h6">Mails</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </q-tab-panel>
-
-        <q-tab-panel name="alarms">
-          <div class="text-h6">Alarms</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </q-tab-panel>
-
-        <q-tab-panel name="movies">
-          <div class="text-h6">Movies</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </q-tab-panel>
-      </q-tab-panels>
-    </q-card>
+    <MoreAboutMe />
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 
 import HeroSection from "components/HeroSection.vue";
 import AboutMe from "components/AboutMe";
+import MoreAboutMe from "components/MoreAboutMe";
 
 export default defineComponent({
   name: "PageIndex",
@@ -45,14 +21,7 @@ export default defineComponent({
   components: {
     HeroSection,
     AboutMe,
-  },
-
-  setup() {
-    const tab = ref("mails");
-
-    return {
-      tab,
-    };
+    MoreAboutMe,
   },
 });
 </script>
