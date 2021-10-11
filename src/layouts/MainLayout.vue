884966d10999ab1790fa6ae2ba6cff413d34bc00
leen-neel <and24903@gmail.com>
2021-10-11 15:47:16 +0530

    fixed color and added toolbar; added button to toggle between light and dark mode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,16 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <!-- <q-header>
-      <q-toolbar class="bg-primary text-white">
-        <q-btn flat round dense icon="menu" class="q-mr-sm" />
-        <q-avatar>
-          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-        </q-avatar>
-
-        <q-toolbar-title>Quasar Framework</q-toolbar-title>
-
-        <q-btn flat round dense icon="whatshot" />
+    <q-header>
+      <q-toolbar class="flex justify-end">
+        <q-btn
+          flat
+          round
+          dense
+          :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'"
+          @click="$q.dark.toggle()"
+        />
       </q-toolbar>
-    </q-header> -->
+    </q-header>
 
     <q-page-container>
       <router-view />
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,7 +14,7 @@
         </div>
 
         <div>
-          <q-avatar size="300px" :class="{ 'avatar-card': $q.screen.lt.md }">
+          <q-avatar size="250px" :class="{ 'avatar-card': $q.screen.lt.md }">
             <img src="images/neel.jpg" />
           </q-avatar>
         </div>
