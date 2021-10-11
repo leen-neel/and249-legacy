21f9f3b705fd3ebf369049960e3b4b9470359a0f
leen-neel <and24903@gmail.com>
2021-10-11 18:20:52 +0530

    added content on the about me section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,10 +4,18 @@
       <div class="allign-left text-bold text-h3">About me</div>
     </q-card-section>
     <q-card-section class="about-text">
-      Hey there! I am
-      <span class="archDau text-bold"> Anindo Neel Dutta! </span> I am a project
-      manager at Norian Games Pvt. Ltd. I make web apps with the
-      <strong>MEVN</strong> stack. I love creating web apps and websites.
+      <p>
+        Hey there! I am
+        <span class="archDau text-bold"> Anindo Neel Dutta! </span> I hail from
+        🇮🇳. I am a project manager at Norian Games Pvt. Ltd. I make web apps
+        with the <strong>MEVN</strong> stack.
+      </p>
+
+      <p>
+        I've been coding since the age of 6. Programming has been an
+        indistinguishable part of my life. Learning about new technology and
+        building stuff has always been a passion of mine.
+      </p>
     </q-card-section>
   </q-card>
 </template>
