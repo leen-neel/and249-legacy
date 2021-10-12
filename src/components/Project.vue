2de602e49b713365be17b01e44ac04e60bbdf437
leen-neel <and24903@gmail.com>
2021-10-12 14:00:21 +0530

    added project component

---
+++ src/components/Project.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-card class="my-card">
+    <q-img
+      :src="`images/projects/${img}`"
+      :ratio="1 / 1"
+      spinner-color="white"
+      spinner-size="82px"
+    />
+    <q-card-section>
+      <div class="text-h6">{{ title }}</div>
+    </q-card-section>
+    <q-card-section class="about-text">
+      <strong>Tech Stack:</strong> {{ stack }}
+
+      <q-btn color="white" label="Learn more" flat />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    stack: String,
+    url: String,
+    img: String,
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.q-card {
+  background-color: #389787;
+}
+
+.body--light {
+  .q-card {
+    background-color: #5dbdad;
+  }
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,17 +4,44 @@
       <div class="oswald allign-left text-bold text-h3">Projects</div>
     </q-card-section>
     <q-card-section class="row">
-      <div class="col-xs-12 col-sm-6 col-md-4 q-pa-sm">
-        <q-card class="my-card">
-          <q-card-section>
-            <div class="text-h6">Our Changing Planet</div>
-            <div class="text-subtitle2">by John Doe</div>
-          </q-card-section>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </q-card-section>
-        </q-card>
+      <div
+        v-for="project in projects"
+        :key="project.title"
+        class="col-xs-12 col-sm-6 col-md-4 q-pa-sm"
+      >
+        <Project
+          :title="project.title"
+          :img="project.img"
+          :stack="project.stack"
+          :url="project.url"
+        />
       </div>
     </q-card-section>
   </q-card>
 </template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+import Project from "./Project.vue";
+
+export default defineComponent({
+  components: {
+    Project,
+  },
+  setup() {
+    const projects = ref([
+      {
+        title: "Katie Jo Pockat",
+        stack: "WordPress Along with Elementor",
+        url: "https://katiejopockat.com/",
+        img: "katiejo.jpg",
+      },
+    ]);
+
+    return {
+      projects,
+    };
+  },
+});
+</script>
