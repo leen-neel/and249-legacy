a60b0e589840fd98c63bb41684b4664ad04190bc
leen-neel <and24903@gmail.com>
2021-10-12 13:41:52 +0530

    added projects section

---
+++ src/components/Projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-card class="q-mt-lg">
+    <q-card-section>
+      <div class="oswald allign-left text-bold text-h3">Projects</div>
+    </q-card-section>
+    <q-card-section class="row">
+      <div class="col-xs-12 col-sm-6 col-md-4 q-pa-sm">
+        <q-card class="my-card">
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
