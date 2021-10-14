ed16b747cf3f5b2a475b8fc2edb0257816a9a555
leen-neel <and24903@gmail.com>
2021-10-14 09:49:27 +0530

    now adding projects thru the api works

--- api/routes/projectAPI.js
+++ api/routes/projectAPI.js
@@ -10,4 +10,48 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const body = req.body;
+
+  if (body.forClient) {
+    const newProject = new Project({
+      title: body.title,
+      stack: body.stack,
+      url: body.stack,
+      img: body.img,
+      forClient: body.forClient,
+      testimonial: body.testimonial,
+      clientName: body.clientName,
+      clientImg: body.clientImg,
+      clientTitle: body.clientTitle,
+    });
+
+    try {
+      const savedPrj = await newProject.save();
+      res.json(savedPrj);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  } else {
+    const newProject = new Project({
+      title: body.title,
+      stack: body.stack,
+      url: body.stack,
+      img: body.img,
+      forClient: body.forClient,
+      description: body.description,
+    });
+
+    try {
+      const savedPrj = await newProject.save();
+      res.json(savedPrj);
+    } catch (error) {
+      console.log(error);
+
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+});
+
 module.exports = router;
