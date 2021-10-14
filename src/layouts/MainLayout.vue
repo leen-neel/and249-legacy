4dc413f29207feb1e87985405f87b9e2b3c01df1
leen-neel <and24903@gmail.com>
2021-10-14 18:15:43 +0530

    added social buttons to header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,6 +13,55 @@
           :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'"
           @click="$q.dark.toggle()"
         />
+
+        <q-separator spaced inset vertical />
+
+        <q-btn
+          type="a"
+          href="https://github.com/leen-neel/"
+          target="_blank"
+          color="white"
+          icon="fab fa-github"
+          flat
+          round
+        >
+          <q-tooltip> GitHub </q-tooltip>
+        </q-btn>
+        <q-btn
+          type="a"
+          href="https://instagram.com/and_249/"
+          target="_blank"
+          color="white"
+          icon="fab fa-instagram"
+          flat
+          round
+        >
+          <q-tooltip> Instagram </q-tooltip>
+        </q-btn>
+
+        <q-btn
+          type="a"
+          href="https://www.linkedin.com/in/anindo-neel-dutta-2a8050199/"
+          target="_blank"
+          color="white"
+          icon="fab fa-linkedin"
+          flat
+          round
+        >
+          <q-tooltip> LinkedIn </q-tooltip>
+        </q-btn>
+
+        <q-btn
+          type="a"
+          href="https://open.spotify.com/playlist/6hJBXl2LTwxuciQLlcse51"
+          target="_blank"
+          color="white"
+          icon="fab fa-spotify"
+          flat
+          round
+        >
+          <q-tooltip> My Spotify Playlist </q-tooltip>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
