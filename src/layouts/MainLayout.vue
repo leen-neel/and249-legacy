0b7cd4944afd89fe03864e9962149670ce9bb9bc
leen-neel <and24903@gmail.com>
2022-03-09 18:05:03 +0530

    removed dark mode/light mode btn

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,14 +5,6 @@
         <q-toolbar-title class="archDau">
           <a href="#top" class="header-title">Anindo Neel Dutta</a>
         </q-toolbar-title>
-
-        <q-btn
-          flat
-          round
-          dense
-          :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'"
-          @click="$q.dark.toggle()"
-        />
       </q-toolbar>
     </q-header>
 
