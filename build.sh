2f2748a27242d2ca3c31a5eac29a14f201feebc3
leen-neel <and24903@gmail.com>
2023-06-27 16:25:23 +0530

    build script updated

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 FILE=./site.zip
 if test -f "$FILE"; then
+    echo "removing old build file"
     rm ./site.zip
 fi
 
 quasar build -m spa
 cd ./dist/spa
-zip -r ../site.zip ./*
+zip -r ../../site.zip ./*
 
 figlet "Site built!"
-
-
+sleep 3
+clear
